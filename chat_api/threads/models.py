07d7062f62bac8c2018aedebdc8657a9aded5287
Lungsangg <lungsang@esukhia.org>
2025-12-17 15:51:04 +0545

    application_table_created

--- chat_api/threads/models.py
+++ chat_api/threads/models.py
@@ -11,8 +11,7 @@ from chat_api.db.db import Base
 
 class DeviceType(enum.Enum):
     web = "web"
-    mobile_app = "mobile_app"
-    
+    mobile_app = "mobile_app"    
 
 class Thread(Base):
     __tablename__ = "threads"
@@ -23,6 +22,7 @@ class Thread(Base):
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
     is_deleted = Column(Boolean, default=False, nullable=False)
     device_type = Column(Enum(DeviceType), nullable=False)
-    application_id = Column(UUID(as_uuid=True), ForeignKey="application_id")
+    application_id = Column(UUID(as_uuid=True), ForeignKey("applications.id"))
+    application = relationship("Application", back_populates="threads")
 
     chats = relationship("Chat", back_populates="thread")
