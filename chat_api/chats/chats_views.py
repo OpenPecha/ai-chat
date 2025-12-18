ac38f00204097b18c6a0ab1e534ae72c782d969e
lobsam <sanduplobzang@gmail.com>
2025-12-18 16:24:51 +0530

    fix

--- chat_api/chats/chats_views.py
+++ chat_api/chats/chats_views.py
@@ -3,6 +3,9 @@ from fastapi.responses import StreamingResponse
 
 from chat_api.chats.chats_services import get_chat_stream
 from chat_api.chats.chats_reponse_model import ChatRequest
+from chat_api.config import get
+from fastapi import HTTPException
+from chat_api.error_contant import ErrorConstant, ResponseError
 
 
 
@@ -13,6 +16,11 @@ chats_router = APIRouter(
 
 @chats_router.post("")
 async def get_chats(chat_request: ChatRequest):
+
+    max_query_length = get("MAX_QUERY_LENGTH")
+    if len(chat_request.query) > int(max_query_length):
+        raise HTTPException(status_code=400, detail=ResponseError(error=ErrorConstant.BAD_REQUEST, message=ErrorConstant.MAX_QUERY_LENGTH_ERROR).model_dump())
+    
     return StreamingResponse(
         get_chat_stream(chat_request),
         media_type="text/event-stream"
