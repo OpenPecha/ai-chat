dd922deea485a367d8ab2c0a20a27aa7bd54a1d3
Lungsangg <lungsang@esukhia.org>
2025-12-22 11:46:33 +0545

    feat: add thread router to main application

--- chat_api/app.py
+++ chat_api/app.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
 from chat_api.views.hello import router as hello_router
+from chat_api.threads.thread_views import thread_router
 
 api = FastAPI(title="ai-chat")
 api.include_router(hello_router)
-
-
+api.include_router(thread_router)
