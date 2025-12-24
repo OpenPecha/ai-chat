6f7ed63693b1e157de4680ee0dfe85f6688a6d66
lobsam <sanduplobzang@gmail.com>
2025-12-24 13:16:41 +0530

    feat: add application creation functionality and integrate applications router

--- chat_api/applications/applications_repository.py
+++ chat_api/applications/applications_repository.py
@@ -4,6 +4,14 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from chat_api.applications.models import Application
+from chat_api.applications.applications_response_models import ApplicationCreateRequest
 
 def get_application_by_name(db: Session, name: str) -> Application:
     return db.execute(select(Application).where(Application.name == name)).scalar_one()
+
+def create_application_repo(db: Session, application: ApplicationCreateRequest) -> Application:
+    db_application = Application(name=application.name)
+    db.add(db_application)
+    db.commit()
+    db.refresh(db_application)
+    return db_application
