afd5a04df88dc7d7681c87a969bd7a38fa7d4683
lobsam <sanduplobzang@gmail.com>
2025-12-24 10:35:34 +0530

    feat: enhance chat streaming by processing response chunks and collecting chat data

--- chat_api/chats/chats_services.py
+++ chat_api/chats/chats_services.py
@@ -1,5 +1,7 @@
 from chat_api.config import get
 import httpx
+import json
+
 from chat_api.chats.chats_reponse_model import ChatRequest, ChatUserQuery, chatRequestPayload
 from chat_api.error_contant import ErrorConstant,ResponseError
 from fastapi import HTTPException
@@ -10,10 +12,17 @@ async def get_chat_stream(chat_request: ChatRequest):
     user_query = ChatUserQuery(role="user", content=chat_request.query)
     chat_request_payload = chatRequestPayload(messages=[user_query]).model_dump()
     url = get("OPENPECHA_AI_URL")
+    chat_list = []
     
     async with httpx.AsyncClient(timeout=httpx.Timeout(60.0, read=120.0)) as client:
         async with client.stream("POST", url, json=chat_request_payload) as response:
             # Stream the response chunks
-            async for chunk in response.aiter_bytes():
+            async for chunk in response.aiter_lines():
                 if chunk:
+                    stripped_chunk = chunk[len("data:"):].strip()
+                    obj = json.loads(stripped_chunk)
+                    if obj is not None:
+                        chat_list.append(obj)
                     yield chunk
+    print(chat_list)
+
