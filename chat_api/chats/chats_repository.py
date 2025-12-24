c15d15bb44a7c64e9da90e19b0e6458cf6101548
lobsam <sanduplobzang@gmail.com>
2025-12-24 12:57:09 +0530

    refactor: update save_chat function to accept a database session parameter

--- chat_api/chats/chats_repository.py
+++ chat_api/chats/chats_repository.py
@@ -1,8 +1,8 @@
 from chat_api.chats.models import Chat
-from chat_api.db import SessionLocal
 from chat_api.chats.chats_reponse_model import ChatResponsePayload
+from sqlalchemy.orm import Session
 
-def save_chat(response_payload: ChatResponsePayload):
+def save_chat(db: Session, response_payload: ChatResponsePayload):
     db.add(response_payload)
     db.commit()
     db.refresh(response_payload)
