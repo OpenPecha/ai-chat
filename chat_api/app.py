aeece1abf35e9d1108510cd33bf9e2c625475b54
TenzDelek <tibetdelek@gmail.com>
2025-12-26 22:52:04 +0530

    cors

--- chat_api/app.py
+++ chat_api/app.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from chat_api.views.hello import router as hello_router
 from chat_api.threads.thread_views import thread_router
 from chat_api.chats.chats_views import chats_router
 from chat_api.applications.application_views import applications_router
 
 api = FastAPI(title="ai-chat")
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 api.include_router(hello_router)
 api.include_router(thread_router)
 api.include_router(chats_router)
